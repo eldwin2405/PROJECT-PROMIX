024548d055523560e3bbd1177e17fe98e0dde32b
eldwin2405 <79311629+eldwin2405@users.noreply.github.com>
2026-05-04 21:39:58 +0800

    Update halaman_utama.py

--- halaman_utama.py
+++ halaman_utama.py
@@ -976,7 +976,7 @@ st.markdown("""
 <style>
 .dev-footer {
     position: fixed;
-    left: 720px;
+    left: 680px;
     bottom: 12px;
     z-index: 9999;
     background: rgba(255, 255, 255, 0.92);
